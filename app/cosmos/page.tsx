f1d9c8dcd1454d3e0563b12eca17976bf99bac2e
Taehee Kim <rabolution@gmail.com>
2023-05-21 20:57:59 +0900

    chore: project setup

---
+++ app/cosmos/page.tsx
@@ -0,0 +1,4 @@
+import { nextCosmosRenderer } from 'react-cosmos-next';
+import * as cosmosImports from '../../cosmos.imports';
+
+export default nextCosmosRenderer(cosmosImports);
---
+++ app/hello.fixture.tsx
@@ -0,0 +1,7 @@
+export default (
+  <div className="p-2">
+		<button className="text-4xl rounded-3xl py-2 px-4 bg-red-400 text-white hover:bg-red-500 transition-colors ease-in-out">
+			행동하기!
+		</button>
+	</div>
+);
